a3aebf71a4449cf16e6bcf33747bf84300e2d6df
Arnaud Avocat Gros <a.avocatg@gmail.com>
2022-04-02 11:44:01 +0300

    add necessary code

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 'use strict';
 import express  from 'express';
+import stationRoute from './routes/stationRoute'
+import authRoute from './routes/authRoute'
+import passport from './utils/pass';
 const app = express();
 const port = process.env.PORT || 8080;
 
+app.use(passport.initialize());
 
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: false })) // for parsing application/x-www-form-urlencoded
