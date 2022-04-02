c85f224a41122f3cf9882895bdd0a6a8a41238e3
Arnaud Avocat Gros <a.avocatg@gmail.com>
2022-04-02 11:06:48 +0300

    deleted all dependencies except express

--- app.js
+++ app.js
@@ -1,18 +1,12 @@
 'use strict';
 import express  from 'express';
-import stationRoute from './routes/stationRoute'
-import authRoute from './routes/authRoute'
-import passport from './utils/pass';
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(passport.initialize());
 
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: false })) // for parsing application/x-www-form-urlencoded
 
-app.use('/station', stationRoute);/*, passport.authenticate('jwt', {session: false})*/
-app.use('/auth', authRoute);
 app.get('/', (req, res) => {
   res.send("Hello chargemap!")
 })
